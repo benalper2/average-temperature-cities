cd87b43b7c02ce44afeb146860466129797b8992
benalper2 <ben_alper@live.com>
2025-03-05 17:43:42 +0000

    onKeyDown changed to onBlur

--- src/App.jsx
+++ src/App.jsx
@@ -30,48 +30,48 @@ export default function App() {
   ]);
 
   const handleKeyDownMin = (event) => {
-    if (event.key === "Enter") {
-      console.log("Type is " + typeof event.target.value);
-      if (isNaN(event.target.value)) {
-        setMinTemp(-99);
-        console.log("NaN");
+    // if (event.key === "Enter") {
+    console.log("Type is " + typeof event.target.value);
+    if (isNaN(event.target.value)) {
+      setMinTemp(-99);
+      console.log("NaN");
+    } else {
+      let num = Number(event.target.value);
+      if (num < -99) {
+        console.log("Less than -99");
+      } else if (num > 99) {
+        console.log("More than 99");
+      } else if (num > maxTemp) {
+        console.log("More than MaxTemp");
       } else {
-        let num = Number(event.target.value);
-        if (num < -99) {
-          console.log("Less than -99");
-        } else if (num > 99) {
-          console.log("More than 99");
-        } else if (num > maxTemp) {
-          console.log("More than MaxTemp");
-        } else {
-          console.log("do validate " + num);
-          setMinTemp(num);
-        }
+        console.log("do validate " + num);
+        setMinTemp(num);
       }
     }
+    // }
   };
 
   const handleKeyDownMax = (event) => {
-    if (event.key === "Enter") {
-      console.log("Type is " + typeof event.target.value);
-      if (isNaN(event.target.value)) {
-        setMaxTemp(99);
-        console.log("NaN");
+    // if (event.key === "Enter") {
+    console.log("Type is " + typeof event.target.value);
+    if (isNaN(event.target.value)) {
+      setMaxTemp(99);
+      console.log("NaN");
+    } else {
+      let num = Number(event.target.value);
+      console.log("num is:  " + num);
+      if (num < -99) {
+        console.log("Less than -99");
+      } else if (num > 99) {
+        console.log("More than 99");
+      } else if (num < minTemp) {
+        console.log("Less than MaxTemp");
       } else {
-        let num = Number(event.target.value);
-        console.log("num is:  " + num);
-        if (num < -99) {
-          console.log("Less than -99");
-        } else if (num > 99) {
-          console.log("More than 99");
-        } else if (num < minTemp) {
-          console.log("Less than MaxTemp");
-        } else {
-          console.log("do validate " + num);
-          setMaxTemp(num);
-        }
+        console.log("do validate " + num);
+        setMaxTemp(num);
       }
     }
+    // }
   };
 
   return (
@@ -98,7 +98,8 @@ export default function App() {
           className="search"
           type="text"
           placeholder="Min"
-          onKeyDown={handleKeyDownMin}
+          onChange={handleKeyDownMin}
+          // onKeyDown={handleKeyDownMin}
         />
         {/* <div style={{ fontSize: 25, fontWeight: "bold" }}>Max</div> */}
         <div
@@ -117,7 +118,8 @@ export default function App() {
           className="search"
           type="text"
           placeholder="Max"
-          onKeyDown={handleKeyDownMax}
+          // onKeyDown={handleKeyDownMax}
+          onChange={handleKeyDownMax}
         />
       </div>
       <div className="City-Name">
